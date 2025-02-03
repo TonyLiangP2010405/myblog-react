776ec1b34e9c666ae948831765e451971f0b8f01
tghrt <m13614526758@163.com>
2025-02-03 15:55:42 +1100

    init project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import './navigation.jsx'
+import Navigation from "./navigation.jsx";
+
+function App() {
+  return (
+              <Navigation />
+  )
+}
+
+export default App
